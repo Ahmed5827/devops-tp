9ffa8a91bd75c8d8c1049495ac316681259f73c6
Chebbi Ahmed <ssjzchebbi@gmail.com>
2025-03-13 11:57:48 +0100

    added /status

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,2 +1,14 @@
 # app .py
-print ("Hello , CI/CD!")
+from flask import Flask
+app = Flask ( __name__ )
+
+@app.route("/")
+def hello ():
+    return "Hello , CI/CD!"
+
+@app.route("/status")
+def hello ():
+    return "Hello , /status!"
+
+if __name__ == " __main__ ":
+    app . run ()
